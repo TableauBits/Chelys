de246b30f41c5a76c1d64936a8443fcab9150cef
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2021-12-22 11:16:27 -0500

    2.2.1

--- src/message.ts
+++ src/message.ts
@@ -14,6 +14,7 @@ export enum EventType {
 	CST_get = "CST-get",
 	CST_get_from_user = "CST-get-from-user",
 	CST_join = "CST-join",
+	CST_state = "CST-state",
 	CST_unsubscribe = "CST-unsubscribe",
 
 	CST_SONG_add = "CST-SONG-add",
