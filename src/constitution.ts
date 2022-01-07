b7ca01416ea65d84d9bac7496568b270af52e513
Ithyx <lamidey.m@gmail.com>
2022-01-06 19:08:20 -0500

    Ajout de l'enum d'état pour grade et fonction pour savoir si on peut voir les résultats

--- src/constitution.ts
+++ src/constitution.ts
@@ -1,98 +1,110 @@
 // TYPE / CONST
 
+import { GradeState } from ".";
+
 export const OWNER_INDEX = 0;
 
 export enum ConstitutionType {
-	GRADE,
-	LENGTH
+    GRADE,
+    LENGTH
 }
 
 export enum AnonymousLevel {
-	PUBLIC,
-	NO_USERNAME,
-	SOUND_ONLY
+    PUBLIC,
+    NO_USERNAME,
+    SOUND_ONLY
 }
 
 export interface Constitution {
-	id: string;
-	season: number;
-	part: number;
-	name: string;
-	isPublic: boolean;
-	anonymousLevel: AnonymousLevel;
-	type: ConstitutionType;
-	state: number;
-	playlistLink: string;
+    id: string;
+    season: number;
+    part: number;
+    name: string;
+    isPublic: boolean;
+    anonymousLevel: AnonymousLevel;
+    type: ConstitutionType;
+    state: number;
+    playlistLink: string;
 
-	users: string[];                // user[0] as owner
-	maxUserCount: number;
-	numberOfSongsPerUser: number;
+    users: string[];                // user[0] as owner
+    maxUserCount: number;
+    numberOfSongsPerUser: number;
 }
 
-export const EMPTY_CONSTITUTION: Constitution =  {
-  id : '',
-  name: '',
-  season: -1,
-  part: -1,
-  isPublic: false,
-  anonymousLevel: -1,
-  maxUserCount: -1,
-  type: -1,
-  state: -1,
-  numberOfSongsPerUser: -1,
-  playlistLink: '',
-  users: []
+export const EMPTY_CONSTITUTION: Constitution = {
+    id: '',
+    name: '',
+    season: -1,
+    part: -1,
+    isPublic: false,
+    anonymousLevel: -1,
+    maxUserCount: -1,
+    type: -1,
+    state: -1,
+    numberOfSongsPerUser: -1,
+    playlistLink: '',
+    users: []
 }
 
 // FUNCTION
 
 export function canModifySongs(constitution: Constitution): boolean {
-	switch (constitution.type) {
-		case ConstitutionType.GRADE: return constitution.state === 0;	// TODO : Magic Number
-	
-		default:
-			return false;
-	}
+    switch (constitution.type) {
+        case ConstitutionType.GRADE: return constitution.state === GradeState.SONG_ADD;
+
+        default:
+            return false;
+    }
 }
 
 export function canModifyVotes(constitution: Constitution): boolean {
-	switch (constitution.type) {
-		case ConstitutionType.GRADE: return constitution.state < 1; // TODO : Magic Number
-		default:
-			return false;
-	}
+    switch (constitution.type) {
+        case ConstitutionType.GRADE: return constitution.state <= GradeState.VOTING;
+
+        default:
+            return false;
+    }
+}
+
+export function areResultsPublic(constitution: Constitution): boolean {
+    switch (constitution.type) {
+        case ConstitutionType.GRADE: return constitution.state === GradeState.RESULTS;
+
+        default:
+            return false;
+    }
 }
 
 // REQUESTS / RESPONSES
 
 export interface CstReqGet {
-	ids: string[];
+    ids: string[];
 }
 
 export interface CstReqCreate {
-	cstData: Constitution;
+    cstData: Constitution;
 }
 
 export interface CstReqJoin {
-	id: string;
+    id: string;
 }
 
 export interface CstReqState {
-	state: number;
-	id: string;
+    state: number;
+    id: string;
 }
 
 export interface CstReqUnsubscribe {
-	ids: string[];
+    ids: string[];
 }
 
 export interface CstResUpdate {
-	cstInfo: Constitution;
+    cstInfo: Constitution;
 }
 
 export type CstReqGetFromUser = {}
 
 export interface CstReqUpdateState {
-	id: string;
-	newState: number;
+    id: string;
+    newState: number;
 }
--- src/grade.ts
+++ src/grade.ts
@@ -1,60 +1,66 @@
 // TYPE / CONST
 
 export interface GradeVote {
-	songId: number;
-	grade: number;
+    songId: number;
+    grade: number;
 }
 
 export interface GradeUserData {
-	uid: string;
-	values: Map<number, number>;
+    uid: string;
+    values: Map<number, number>;
 }
 
 export interface GradeSummary {
-	voteCount: number;
-	userCount: Map<string, number>
+    voteCount: number;
+    userCount: Map<string, number>
+}
+
+export enum GradeState {
+    SONG_ADD = 0,
+    VOTING = 1,
+    RESULTS = 2,
 }
 
 // Interface use in Kalimba and compatible with Firebase and JSON
 
 export interface KGradeUserData {
-	uid: string;
-	values: Record<string, unknown>;
+    uid: string;
+    values: Record<string, unknown>;
 }
 
 export interface KGradeSummary {
-	voteCount: number;
-	userCount: Record<string, number>
+    voteCount: number;
+    userCount: Record<string, number>
 }
 
 // REQUESTS / RESPONSES
 
 export interface GradeReqEdit {
-	cstId: string,
-	voteData: GradeVote
+    cstId: string,
+    voteData: GradeVote
 }
 
 export interface GradeReqGetAll {
-	cstId: string;
+    cstId: string;
 }
 
 export interface GradeReqGetUser {
-	cstId: string;
+    cstId: string;
 }
 
 export interface GradeReqGetSummary {
-	cstId: string;
+    cstId: string;
 }
 
 export interface GradeReqUnsubscribe {
-	cstId: string;
+    cstId: string;
 }
 
 export interface GradeResSummaryUpdate {
-	summary: KGradeSummary;
+    summary: KGradeSummary;
 }
 
 export interface GradeResUserDataUpdate {
-	status: "added" | "modified" | "removed";
-	userData: KGradeUserData;
+    status: "added" | "modified" | "removed";
+    userData: KGradeUserData;
 }
