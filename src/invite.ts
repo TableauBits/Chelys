034b02e5fa7a4876b32153ae3c0b8841ed1949af
Ithyx <lamidey.m@gmail.com>
2022-08-18 13:27:30 -0400

    2.7.4

--- src/invite.ts
+++ src/invite.ts
@@ -3,32 +3,40 @@ import { ResponseStatus } from "./client";
 // TYPE / CONST
 
 export interface Invite {
-  id: string;
-  createdBy: string;
-  date: string;
+	id: string;
+	createdBy: string;
+	date: string;
+}
+
+export interface NewAccount {
+	uid: string;
+	displayName: string;
+	email: string;
+	photoURL: string;
 }
 
 // REQUESTS / RESPONSES
 
-export interface InvReqNew {}
-
-export interface InvReqGetAll {}
-
-export interface InvRefCheck{
-  id: string;
-}
+export interface InvReqNew { }
 
 export interface InvReqDelete {
-  id: string;
+	id: string;
 }
+
+export interface InvReqGetAll { }
+
+export interface InvReqCheck {
+	id: string;
+	account: NewAccount;
+}
+
+export interface InvReqUnsubscribe { }
 
 export interface InvResCheck {
-  status: ResponseStatus;
+	status: ResponseStatus;
 }
 
-export interface InvResUnsubscribe {}
-
 export interface InvResUpdate {
-  status: "added" | "modified" | "removed";
-  invite: Invite;
+	status: "added" | "modified" | "removed";
+	invite: Invite;
 }
